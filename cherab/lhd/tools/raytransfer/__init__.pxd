850e0223a619f5ff868c6b0458997f4f0cc54b91
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2021-08-29 14:04:19 +0900

    1st comitt of Raytransfer emitters for EMC3

---
+++ cherab/lhd/tools/raytransfer/__init__.pxd
@@ -0,0 +1,3 @@
+# cython: language_level=3
+
+from cherab.lhd.tools.raytransfer.emitters import .
---
+++ cherab/lhd/tools/raytransfer/__init__.py
@@ -0,0 +1,2 @@
+from cherab.lhd.tools.raytransfer import .
+from cherab.lhd.tools.raytransfer.emitters import .
