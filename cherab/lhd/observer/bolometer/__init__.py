ac3251394b9e83ed3c6786535eb3235e239afdff
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2022-03-24 21:58:54 +0900

    [remove] resistive module
    change to use original bolometry module
    and remove comments in json file

--- cherab/lhd/observer/bolometer/__init__.py
+++ cherab/lhd/observer/bolometer/__init__.py
@@ -1,12 +1,9 @@
 from .irvb import IRVBCamera
-from .resistive import BolometerCamera, BolometerFoil
 from .load_irvb import load_irvb, load_irvb_as_pinhole_camera, calcam_virtual_calibration
 from .load_resistive import load_resistive
 
 __all__ = [
     "IRVBCamera",
-    "BolometerCamera",
-    "BolometerFoil",
     "load_irvb",
     "load_irvb_as_pinhole_camera",
     "calcam_virtual_calibration",
