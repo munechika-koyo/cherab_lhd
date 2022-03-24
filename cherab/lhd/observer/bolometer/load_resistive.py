6453ad723dbfcc0e4dc3555b4e946c1ed73ac90d
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2022-03-24 23:16:59 +0900

    [update] load resistive bolometer func

--- cherab/lhd/observer/bolometer/load_resistive.py
+++ cherab/lhd/observer/bolometer/load_resistive.py
@@ -1,19 +1,14 @@
 import os
 from json import load
-import numpy as np
-from raysect.core.math import Point3D, Vector3D, translate, rotate_basis, rotate_vector, rotate_z
+from raysect.core import World
+from raysect.core.math import Point3D
 from raysect.primitive import Box, Subtract
 from raysect.optical.material import AbsorbingSurface
-from cherab.tools.observers import BolometerSlit
-from .resistive import BolometerCamera, BolometerFoil
+from cherab.tools.observers import BolometerCamera, BolometerSlit, BolometerFoil
+from .load_irvb import _centre_basis_and_dimensions
 
 
 DATA_DIRECTORY = os.path.join(os.path.dirname(__file__), "data")
-
-XAXIS = Vector3D(1, 0, 0)
-YAXIS = Vector3D(0, 1, 0)
-ZAXIS = Vector3D(0, 0, 1)
-ORIGIN = Point3D(0, 0, 0)
 
 
 def load_resistive(port="6.5L", model_variant="I", parent=None):
@@ -32,12 +27,66 @@ def load_resistive(port="6.5L", model_variant="I", parent=None):
 
     Returns
     -------
-    :obj:`.BolometerCamera`
-        populated :obj:`.BolometerCamera` instance
+    :obj:`~cherab.tools.observers.bolometry.BolometerCamera`
+        populated :obj:`~cherab.tools.observers.bolometry.BolometerCamera.BolometerCamera` instance
     """
     # Load configuration from .json file
     file = os.path.join(DATA_DIRECTORY, "RB.json")
     with open(file=file, mode="r") as f:
         raw_data = load(f)
-    
-    return None
+
+    # extract user-specified IRVB model
+    try:
+        raw_data = raw_data[port][model_variant]
+    except KeyError:
+        raise KeyError(f"spcified parameters: {port} or {model_variant} are not defined.")
+
+    # ----------------------------------------------------------------------- #
+    #  Build Bolometer Camera
+    # ----------------------------------------------------------------------- #
+
+    bolometer_camera = BolometerCamera(parent=parent, name=f"LHD-RB-{port}-{model_variant}")
+
+    slit_centre, slit_basis_x, slit_basis_y, width, height = _centre_basis_and_dimensions(
+        [Point3D(*slit) for slit in raw_data["slit"]]
+    )
+    slit = BolometerSlit(
+        slit_id="slit", centre_point=slit_centre,
+        basis_x=slit_basis_x, dx=width, basis_y=slit_basis_y, dy=height,
+        parent=bolometer_camera
+    )
+
+    for id, foil_geom in raw_data["foil"].items():
+        foil_geometry = _centre_basis_and_dimensions([Point3D(*foil_xyz) for foil_xyz in foil_geom])
+        centre, basis_x, basis_y, width, height = foil_geometry
+        foil = BolometerFoil(
+            detector_id=f"Foil {id}",
+            centre_point=centre,
+            basis_x=basis_x, dx=width,
+            basis_y=basis_y, dy=height,
+            slit=slit, parent=bolometer_camera, units="Power",
+            accumulate=False
+        )
+        bolometer_camera.add_foil_detector(foil)
+
+    # ----------------------------------------------------------------------- #
+    #  Create CSG aperture
+    # ----------------------------------------------------------------------- #
+    width = max(slit.dx, slit.dy) * 2.0
+    face = Box(Point3D(-width, -width, -slit.dz * 0.5), Point3D(width, width, slit.dz * 0.5))
+    slit_box = Box(
+        lower=Point3D(-slit.dx * 0.5, -slit.dy * 0.5, -slit.dz * 0.6),
+        upper=Point3D(slit.dx * 0.5, slit.dy * 0.5, slit.dz * 0.6)
+    )
+    _ = Subtract(
+        face, slit_box, parent=slit,
+        material=AbsorbingSurface(), name=f"{slit.name} - CSG Aperture"
+    )
+
+    return bolometer_camera
+
+
+if __name__ == "__main__":
+    world = World()
+    rb = load_resistive(parent=world)
+    pass
