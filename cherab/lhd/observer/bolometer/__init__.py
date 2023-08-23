f1756772d5f7b328ac1492e8268617cfa730f72d
Koyo Munechika <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-08-23 18:06:51 +0900

    🔥 remove an obsolete function

--- cherab/lhd/observer/bolometer/__init__.py
+++ cherab/lhd/observer/bolometer/__init__.py
@@ -1,12 +1,11 @@
 """Bolometer diagnostics modules."""
 from .irvb import IRVBCamera
-from .load_irvb import calcam_virtual_calibration, load_irvb, load_irvb_as_pinhole_camera
+from .load_irvb import load_irvb, load_irvb_as_pinhole_camera
 from .load_resistive import load_resistive
 
 __all__ = [
     "IRVBCamera",
     "load_irvb",
     "load_irvb_as_pinhole_camera",
-    "calcam_virtual_calibration",
     "load_resistive",
 ]
