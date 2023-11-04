530045667ad80f7dee6cc1ea44637556e2858419
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-11-04 18:31:03 +0900

    ⚡️ re-code module using new functionalities
    and remove `emc3` because it is loaded in `emc3` subpackage

--- cherab/lhd/emc3/raytransfer/raytransfer.py
+++ cherab/lhd/emc3/raytransfer/raytransfer.py
@@ -1,56 +1,85 @@
-"""This module offers the helper function to easily set raytransfer
-material."""
+"""This module offers the helper function to easily set raytransfer material."""
 from __future__ import annotations
 
 from raysect.core.math import translate
 from raysect.optical import World
 from raysect.primitive import Cylinder
 
-from cherab.lhd.tools import Spinner
+from cherab.lhd.tools.spinner import DummySpinner, Spinner
 
-from ..geometry import TomographyZone
+from ..indices import create_index_func
 from .emitters import Discrete3DMeshRayTransferEmitter
 
-__all__ = ["load_rte_emc3"]
+__all__ = ["load_rte"]
 
 
 # Constants
 RMIN, RMAX = 2.0, 5.5  # [m]
 ZMIN, ZMAX = -1.6, 1.6
+ZONES = [
+    "zone0",
+    "zone1",
+    "zone2",
+    "zone3",
+    "zone4",
+    "zone11",
+    "zone12",
+    "zone13",
+    "zone14",
+    "zone15",
+]
 
 
-@Spinner(text="Loading RayTransfer Emitter...", timer=True)
-def load_rte_emc3(parent: World, bins: int | None = None) -> Cylinder:
-    """Helper function of loding RayTransfer Emitter using
-    :obj:`.Discrete3DMeshRayTransferEmitter`
+def load_rte(
+    parent: World, zones: list[str] = ZONES, index_type: str = "cell", verbose: bool = True
+) -> list[Cylinder]:
+    """Helper function of loding RayTransfer Emitter using :obj:`.Discrete3DMeshRayTransferEmitter`.
+
 
     Parameters
     ----------
     parent
         raysect world Node
-    bins
-        the number of grids, by default ``14 * 18``
+    zones
+        zones of EMC3-EIRENE mesh
+    index_type
+        type of indexing EMC3 grids. The index data must be created in advance using
+        :obj:`~cherab.lhd.emc3.indices.create_new_index`
+    verbose
+        if True, show progress spinner
 
     Returns
     -------
-    :obj:`~raysect.primitive.cylinder.Cylinder`
-        primitive of cylinder
+    list[:obj:`~raysect.primitive.cylinder.Cylinder`]
+        list of primitives of cylinder
     """
 
-    bins = bins or 14 * 18  # 14 zones x 18 degrees
+    emitters = []
+    spinner = Spinner if verbose else DummySpinner
+    base_text = "Loading RayTransfer Emitter "
+    with spinner(text=base_text, timer=True) as sp:
+        # Load index function
+        for zone in zones:
+            sp.text = base_text + f"({zone=}, {index_type=})"
+            index_func, bins = create_index_func(zone=zone, index_type=index_type)
 
-    # Load index function
-    index_func = TomographyZone()
+            # material as emitter
+            material = Discrete3DMeshRayTransferEmitter(index_func, bins, integration_step=0.001)
 
-    # material as emitter
-    material = Discrete3DMeshRayTransferEmitter(index_func, bins, integration_step=0.001)
+            # primitive using cylinder
+            shift = translate(0, 0, ZMIN)
+            emitter = Cylinder(
+                RMAX,
+                ZMAX - ZMIN,
+                transform=shift,
+                parent=parent,
+                material=material,
+                name=f"RayTransferEmitter-{zone}",
+            )
 
-    # primitive using cylinder
-    shift = translate(0, 0, ZMIN)
-    emitter = Cylinder(RMAX, ZMAX - ZMIN, transform=shift, parent=parent, material=material)
+            emitters.append(emitter)
 
-    return emitter
+        sp.text = base_text
+        sp.ok()
 
-
-if __name__ == "__main__":
-    pass
+    return emitters
