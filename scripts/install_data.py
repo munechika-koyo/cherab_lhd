4773bd0b870b641b8a6aa28f7acc29bf8ef4d971
munechika-koyo <munechika.koyo@gmail.com>
2025-02-14 20:10:49 +0100

    ♻️ Refactor install_data script to enhance data installation process and update registry handling

--- scripts/install_data.py
+++ scripts/install_data.py
@@ -1,59 +1,60 @@
-"""This script installs EMC3 data into the specified HDF5 storage.
+"""This script installs EMC3 raw data into the user's cache.
 
 The script takes the following command-line arguments:
 - --data-dir: Directory containing the data files (required)
 - --grid-filename: Filename for the grid data (required)
 - --cell-filename: Filename for the cell data (required)
-- --overwrite: Flag to overwrite existing data (optional)
 
-The script performs the following steps:
-1. Parses the input parameters.
-2. Installs the grid data into the HDF5 storage.
-3. Installs the physical cell indices into the HDF5 storage.
-4. Installs the cell indices into the HDF5 storage.
-5. Installs the remaining data into the HDF5 storage.
+Examples
+--------
+.. code-block:: bash
 
-Usage:
-    pixi python install_data.py --data-dir <data_directory> --grid-filename <grid_file> --cell-filename <cell_file> [--overwrite]
-
-Example:
-    pixi python install_data.py --data-dir /path/to/data --grid-filename grid.h5 --cell-filename cell.h5 --overwrite
+    pixi run python install_data.py --data-dir /path/to/data --grid-filename grid-360.txt --cell-filename CELL_GEO
 """
 
 import argparse
+import json
+from importlib.resources import as_file, files
 from pathlib import Path
 
+from pooch import file_hash
+
 from cherab.lhd.emc3.repository.install import (
-    install_cell_indices,
+    install_center_points,
     install_data,
     install_grids,
-    install_physical_cell_indices,
+    install_indices,
 )
-from cherab.lhd.tools.fetch import PATH_TO_STORAGE
+from cherab.lhd.tools.fetch import PATH_TO_STORAGE, get_registries
 
 # Parse input parameters
 parser = argparse.ArgumentParser(description="Install EMC3 data")
 parser.add_argument("--data-dir", required=True, help="Directory containing the data files")
 parser.add_argument("--grid-filename", required=True, help="Filename for the grid data")
 parser.add_argument("--cell-filename", required=True, help="Filename for the cell data")
-parser.add_argument("--overwrite", action="store_true", help="Overwrite existing data")
 args = parser.parse_args()
 
 data_dir = Path(args.data_dir)
-grid_filename = args.grid_filename
-cell_filename = args.cell_filename
-overwrite = args.overwrite
+grid_filename = Path(args.grid_filename)
+cell_filename = Path(args.cell_filename)
 
-# install grids
-install_grids(
-    data_dir / grid_filename,
-    hdf5_path=PATH_TO_STORAGE / "emc3.hdf5",
-    update=overwrite,
-)
-install_physical_cell_indices(
-    data_dir / cell_filename,
-    hdf5_path=PATH_TO_STORAGE / "emc3.hdf5",
-    update=overwrite,
-)
-install_cell_indices(hdf5_path=PATH_TO_STORAGE / "emc3.hdf5", update=overwrite)
-install_data(data_dir, hdf5_path=PATH_TO_STORAGE / "emc3.hdf5")
+# Install grids. NOTE: must be done first
+install_grids(data_dir / grid_filename)
+
+# Install pre-configured indices
+install_indices(data_dir / cell_filename)
+
+# Install center points
+install_center_points()
+
+# Install remaining data
+install_data(data_dir)
+
+# Update registry keys
+key_registry = f"emc3/{grid_filename.with_suffix('.nc')}"
+hash = file_hash(PATH_TO_STORAGE / key_registry)
+registry = get_registries()
+registry.update({key_registry: hash})
+with as_file(files("cherab.lhd.tools").joinpath("registries.json")) as file:
+    with file.open("w") as f:
+        json.dump(registry, f, indent=2)
